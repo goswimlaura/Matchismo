ab0cc93794d286df9510c6aed1d5ef28f595cab3
goswimlaura <caymoe@gmail.com>
2015-09-02 14:15:56 -0700

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Joseph Gordon on 9/2/15.
+//  Copyright © 2015 Laura Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Joseph Gordon on 9/2/15.
+//  Copyright © 2015 Laura Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
